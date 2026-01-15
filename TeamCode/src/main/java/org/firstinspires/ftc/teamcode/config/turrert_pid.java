6d4723ae024f53c154bb86720d1f50b1a93e029d
sonsw0973 <sonsw0973@naver.com>
2026-01-15 13:42:51 +0900

    son

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/turrert_pid.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.config;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PIDFController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Configurable
+
+@TeleOp
+public class turrert_pid extends OpMode {
+
+    private PIDFController controller;
+
+    public static double p = 0, i = 0, d = 0;
+    public final double f = 0;
+    public static double target_deg = 0;
+    public double target_tick;
+    public final double ticks_per_rev = 103.8;
+    private PIDFCoefficients pidfCoefficients;
+    private double motor_power;
+
+    DcMotor SA;
+
+
+    @Override
+    public void init() {
+        pidfCoefficients = new PIDFCoefficients(p,i,d,f);
+        controller = new PIDFController(pidfCoefficients);
+
+        SA = hardwareMap.dcMotor.get("SA");
+
+        SA.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        SA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    @Override
+    public void loop() {
+
+        target_tick = (target_deg/360.0) * (105.0/25.0) * ticks_per_rev; //패널에서 목표각도 입력 -> tick으로 변환
+        controller.setTargetPosition(target_tick); //pidf controller 상수 변경
+
+        pidfCoefficients = new PIDFCoefficients(p,i,d,f);
+        controller.setCoefficients(pidfCoefficients);
+
+        double currentPos = SA.getCurrentPosition();
+        controller.updatePosition(currentPos);
+
+        double current_deg = (currentPos) / (ticks_per_rev * (105.0/25.0)) * 360.0;
+
+
+        motor_power = controller.run();
+
+        SA.setPower(motor_power);
+
+        telemetry.addData("target deg: ", target_deg);
+        telemetry.addData("error deg: ", target_deg - current_deg);
+        telemetry.addData("target tick: ", target_tick);
+        telemetry.addData("error tick: ", target_tick- controller.getError());
+        telemetry.update();
+    }
+}
+
